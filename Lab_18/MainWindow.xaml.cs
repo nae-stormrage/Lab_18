3bb8e0f9ea0281376ed386666f6f2531e2c07de1
nae-stormrage <stas.markus.2004@mail.ru>
2024-12-13 21:52:40 +0300

    Добавьте файлы проекта.

---
+++ Lab_18/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab_18
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalc_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                double X = Convert.ToDouble(XTB.Text);
+                double Y = Convert.ToDouble(YTB.Text);
+                int N = Convert.ToInt32(NTB.Text);
+                int K = Convert.ToInt32(KTB.Text);
+                double Summa = 0;
+
+                for (int i = 1; i <= N; i++)
+                {
+                    for (int j = 1; j <= K; j++)
+                    {
+                        Summa += (Math.Cos(Math.Pow(Y, i)) + j * X) / (i * j);
+                    }
+                }
+
+                ResultTB.Text = Summa.ToString();
+            }
+            catch 
+            {
+                ResultTB.Text = "Ошибка! Проблема в цифрах! Решите пж пж!";
+            }
+        }
+    }
+}
